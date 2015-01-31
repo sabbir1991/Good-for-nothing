83516fc5bd8534a0485e725a2ea05629c2842388
Sekander Badsha <badsha_eee@yahoo.com>
2015-01-31 18:53:14 +0600

    completed the plugin

---
+++ sekander.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Sekander Baksho
+ * Plugin URI: http://sekander.pro
+ * Description: Good for nothing
+ * Version: 0.0.1
+ * Author: Basko
+ * Author URI: http://fb.com/sekander.pro
+ * Text Domain: sekander
+ * License: MIT
+ */
+
+ _e ("Hello World", "sekander");
+
+?>
